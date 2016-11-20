06ea9c9670c6bac21f3f0cbb28a2b42810676a78
William Yang <yangzwilliam@gmail.com>
2016-11-19 19:13:45 -0500

    provisioned postgresql database

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ pg.defaults.ssl = true;
 
 
 
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
+
+
 var port_number = process.env.PORT || 8081;
 var server = app.listen(port_number, function () {
   var host = server.address().address
